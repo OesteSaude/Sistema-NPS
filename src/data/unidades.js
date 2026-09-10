2b6043ab0db8a35db974123fb2924d2a21a33427
OesteSaude <marketing@oestesaude.dev.br>
2026-09-10 12:04:37 -0300

    update-unidades

--- src/data/unidades.js
+++ src/data/unidades.js
@@ -1,13 +1,13 @@
 export const UNIDADES = [
-  { id: 'presidente-prudente', nome: 'Presidente Prudente' },
-  { id: 'assis', nome: 'Assis' },
-  { id: 'marilia', nome: 'Marília' },
-  { id: 'bauru', nome: 'Bauru' },
-  { id: 'aracatuba', nome: 'Araçatuba' },
-  { id: 'dracena', nome: 'Dracena' },
-  { id: 'tupa', nome: 'Tupã' },
-  { id: 'ourinhos', nome: 'Ourinhos' },
   { id: 'adamantina', nome: 'Adamantina' },
+  { id: 'bataguassu', nome: 'Bataguassu' },
+  { id: 'campo-grande', nome: 'Campo Grande' },
+  { id: 'dracena', nome: 'Dracena' },
+  { id: 'nova-andradina', nome: 'Nova Andradina' },
+  { id: 'osvaldo-cruz', nome: 'Osvaldo Cruz' },
+  { id: 'presidente-epitacio', nome: 'Presidente Epitácio' },
+  { id: 'presidente-prudente', nome: 'Presidente Prudente' },
+  { id: 'presidente-venceslau', nome: 'Presidente Venceslau' },
 ];
 
 export function findUnidadeById(id) {
